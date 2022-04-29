550978b9b35b65a3631b597393f5bffea0574d4d
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-29 15:19:34 +0600

    Add browserRoute routes route and link

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import "./App.css";
 import Footer from "./Shared/Footer/Footer";
 import Header from "./Shared/Header/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import ManageItems from "./Pages/ManageItems/ManageItems";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route
+          path="/manageItems"
+          element={<ManageItems></ManageItems>}
+        ></Route>
+      </Routes>
       <Footer></Footer>
     </div>
   );
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -10,8 +11,12 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Manage Items</Nav.Link>
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/manageItems">
+                Manage Items
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
