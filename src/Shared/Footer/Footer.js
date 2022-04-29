da83b93a00adfbaf58161628990d371dae7061f1
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-29 15:09:52 +0600

    Update footer design

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -2,9 +2,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="bg-dark text-center">
+      <div className="py-5">
+        <p className="text-white fw-bold">
+          Developed By : <span className="text-primary">Rafat Hossain</span>
+        </p>
+      </div>
+    </footer>
   );
 };
 
