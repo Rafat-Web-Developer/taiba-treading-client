ba91ff4d66ebb49b99cc8dce3e788c7994a362b4
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-29 15:01:19 +0600

    Update navbar design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Footer from "./Shared/Footer/Footer";
 import Header from "./Shared/Header/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <Navbar bg="primary" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Taiba-Treading</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Manage Items</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 };
 
