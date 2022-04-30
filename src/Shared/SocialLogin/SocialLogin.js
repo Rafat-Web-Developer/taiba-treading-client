1fecc6a77f68237cdbe3bba87634aa8fab115ce7
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-01 03:20:15 +0600

    Complete google login system

--- src/Shared/SocialLogin/SocialLogin.js
+++ src/Shared/SocialLogin/SocialLogin.js
@@ -1,15 +1,36 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const SocialLogin = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+  const navigate = useNavigate();
+
+  if (user) {
+    navigate("/manageItems");
+  }
+
+  const handleGoogleLogin = () => {
+    signInWithGoogle();
+  };
+
   return (
     <div>
+      {error && <p className="text-danger">{error?.message}</p>}
       <div className="mb-3 mt-4 d-flex align-items-center justify-content-center">
         <p className="w-25 bg-primary" style={{ height: "2px" }}></p>
         <p className="text-primary fw-bold mx-2">Or</p>
         <p className="w-25 bg-primary" style={{ height: "2px" }}></p>
       </div>
-      <Button variant="success" type="button" className="w-100 fw-bold">
+      <Button
+        variant="success"
+        onClick={handleGoogleLogin}
+        type="button"
+        className="w-100 fw-bold"
+      >
         Google
       </Button>
     </div>
