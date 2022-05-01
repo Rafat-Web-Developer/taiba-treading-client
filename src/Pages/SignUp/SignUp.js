9a45bd583a37e5ea497e9e8d0e4b379594c2ba65
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-01 12:03:03 +0600

    Complete sign up option

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, Container, Form, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import SocialLogin from "../../Shared/SocialLogin/SocialLogin";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+
 const SignUp = () => {
+  const [errorMessage, setErrorMessage] = useState("");
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const navigate = useNavigate();
+
+  const handleSignUpData = (event) => {
+    event.preventDefault();
+
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const confirmPassword = event.target.confirmPassword.value;
+
+    if (password.length <= 5) {
+      setErrorMessage("Password must have at least 6 characters.");
+      return;
+    }
+    if (password !== confirmPassword) {
+      setErrorMessage("Password not matched");
+      return;
+    }
+
+    setErrorMessage("");
+    createUserWithEmailAndPassword(email, password);
+    navigate("/manageItems");
+  };
   return (
     <section className="my-5">
       <Container>
@@ -11,28 +41,46 @@ const SignUp = () => {
             Create your new account
           </Card.Header>
           <Card.Body className="py-4">
-            <Form>
+            <Form onSubmit={handleSignUpData}>
               <Form.Group className="mb-3" controlId="formBasicName">
-                <Form.Control type="text" placeholder="Enter your name" />
+                <Form.Control
+                  type="text"
+                  name="name"
+                  placeholder="Enter your name"
+                  required
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Control type="email" placeholder="Enter your email" />
+                <Form.Control
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  required
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Control
                   type="password"
+                  name="password"
                   placeholder="Enter your password"
+                  required
                 />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
                 <Form.Control
                   type="password"
+                  name="confirmPassword"
                   placeholder="Enter your password again"
+                  required
                 />
               </Form.Group>
+              {error && <p className="text-danger fw-bold">{error.message}</p>}
+              {errorMessage && (
+                <p className="text-danger fw-bold">{errorMessage}</p>
+              )}
               <Button
                 variant="primary"
                 type="submit"
