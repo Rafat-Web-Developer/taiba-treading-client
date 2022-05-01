8c185639f5979e2af931ab720a58e2fccafc91a7
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-02 00:09:07 +0600

    email verification code complete

--- src/Pages/RequireAuth/RequireAuth.js
+++ src/Pages/RequireAuth/RequireAuth.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 import Loading from "../../Shared/Loading/Loading";
 
 const RequireAuth = ({ children }) => {
   const [user, loading] = useAuthState(auth);
   const location = useLocation();
+  const navigate = useNavigate();
 
   if (loading) {
     return <Loading></Loading>;
@@ -14,6 +15,11 @@ const RequireAuth = ({ children }) => {
 
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+  }
+
+  if (!user.emailVerified) {
+    navigate("/emailVerification");
+    return;
   }
 
   return children;
